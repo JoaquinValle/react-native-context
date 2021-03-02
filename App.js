10690013f4dbcc528b271061e33997d0d66f1649
JoaquinValle <joaquinvalle.int@gmail.com>
2021-03-02 17:49:16 -0600

    feature: header navigation

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import "react-native-gesture-handler";
 import React from "react";
+import { Text, Button, TouchableOpacity } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import Svg, { Path } from "react-native-svg";
 
 import { Provider as BlogProvider } from "./src/context/BlogContext";
 
@@ -17,8 +19,54 @@ const App = () => {
     <BlogProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Show" component={ShowScreen} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+                  <Svg
+                    style={{ width: 28, height: 28, marginRight: 8 }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <Path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </Svg>
+                </TouchableOpacity>
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="Show"
+            component={ShowScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Edit")}>
+                  <Svg
+                    style={{ width: 28, height: 28, marginRight: 8 }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <Path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                    />
+                  </Svg>
+                </TouchableOpacity>
+              ),
+            })}
+          />
           <Stack.Screen name="Create" component={CreateScreen} />
           <Stack.Screen name="Edit" component={EditScreen} />
         </Stack.Navigator>
