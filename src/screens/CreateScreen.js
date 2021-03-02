f46fd98680129b7b915ae6f260c060770a995fda
JoaquinValle <joaquinvalle.int@gmail.com>
2021-03-01 22:23:00 -0600

    boilerplate: initial screens

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ShowScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default ShowScreen
+
+const styles = StyleSheet.create({})
---
+++ src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const EditScreen = ({navigation}) => {
+    return (
+        <View>
+            <Text>Show Screen</Text>
+        </View>
+    )
+}
+
+export default EditScreen
+
+const styles = StyleSheet.create({})
