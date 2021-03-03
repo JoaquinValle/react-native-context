55919f08695267dc05b9773d606c3732e8370e34
JoaquinValle <joaquinvalle.int@gmail.com>
2021-03-03 12:05:05 -0600

    fix: api refactor initial

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,16 +4,10 @@ import jsonServer from "../api/jsonServer";
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogpost":
-      return [action.payload];
+      return action.payload;
     case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 999999),
-          title: action.payload.title,
-          body: action.payload.body,
-        },
-      ];
+      console.log(action.payload);
+      return [...state];
     case "delete_blogpost":
       return state.filter((blogPost) => {
         return blogPost.id !== action.payload;
@@ -29,22 +23,43 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return (data, callback) => {
-    dispatch({ type: "add_blogpost", payload: data });
-    callback();
+  return async (data, callback) => {
+    try {
+      const response = await jsonServer.post("/blogposts", {
+        title: data.title,
+        body: data.body,
+      });
+    //   dispatch({ type: "add_blogpost" });
+      console.log(response.status);
+      if (callback) {
+        callback();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 const getBlogPost = (dispatch) => {
   return async () => {
-    const response = await jsonServer.get("/blogposts");
-    dispatch({ type: "get_blogpost", payload: response });
+    try {
+      const response = await jsonServer.get("/blogposts");
+      dispatch({ type: "get_blogpost", payload: response.data });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
-    dispatch({ type: "delete_blogpost", payload: id });
+  return async (id) => {
+    try {
+      const response = await jsonServer.delete(`/blogposts/${id}`);
+      dispatch({ type: "delete_blogpost", payload: id });
+      console.log(response.status);
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
@@ -53,7 +68,7 @@ const editBlogPost = (dispatch) => {
     dispatch({
       type: "edit_blogpost",
       payload: { id, title: data.title, body: data.body },
-    }); 
+    });
     callback();
   };
 };
